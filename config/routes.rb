b669f79c0c562694256f51a884bbee71577bc1a3
Alexa <alexa3000g@outlook.com>
2023-02-28 19:20:45 +0800

    feat: add nest resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :boards
+  resources :boards do
+    resources :posts
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
